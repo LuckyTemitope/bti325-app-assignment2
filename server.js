2b9b7aadca407a883c50bae612186427e96f1dbb
Lucky Osunbiyi <osunbiyi.temitope1@gmail.com>
2024-10-07 02:40:30 -0400

    Updated post.json and categories.json
    Updated the custom blog-service.js module

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require("path");
+const blog = require("blog-service.js");
+const app = express();
+
+const HTTP_PORT = process.env.PORT || 8080;
+
+app.use(express.static("public"));
+
+
+app.get('/', (req, res) => {
+    res.redirect('/about');
+  });
+
+  app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/about.html"));
+});
+
+app.listen(HTTP_PORT, () => { console.log(`server listening on: ${HTTP_PORT}`) });
